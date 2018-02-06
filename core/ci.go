a0e1ae04384b4b8bb0e3f0d24df4d085b44101e8
aong <yxwzaxns@gmail.com>
2018-02-06 17:59:33 +0800

    fix a problem about docker daemon API version

--- core/ci.go
+++ core/ci.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/docker/docker/api/types"
@@ -16,11 +18,12 @@ import (
 )
 
 var (
-	CodeURL  string
-	WorkPath string
-	RootDir  string
-	MChan    chan M
-	m        *M
+	CodeURL          string
+	WorkPath         string
+	RootDir          string
+	MChan            chan M
+	m                *M
+	DockerAPIVersion float64
 )
 
 func init() {
@@ -28,6 +31,7 @@ func init() {
 	WorkPath = ""
 	RootDir = "/tmp"
 	m = new(M)
+	DockerAPIVersion = 1.36
 }
 
 func StartCI(URL string, msgChan chan M) {
@@ -40,7 +44,13 @@ func StartCI(URL string, msgChan chan M) {
 	println("initWorkDir finished")
 	pullCode()
 	println("pullCode finished")
-	buildImage()
+	for {
+		if buildImage() == 0 {
+			break
+		} else {
+			DockerAPIVersion -= 0.01
+		}
+	}
 	println("buildImage finished")
 	checkImage()
 	println("checkImage finished")
@@ -56,6 +66,7 @@ func initWorkDir() {
 		// os.RemoveAll(path)
 		if err := os.MkdirAll(path, 0777); err != nil {
 			println(err.Error())
+			os.Exit(1)
 		}
 	} else {
 		// if dir exist that check if exist .git file
@@ -110,50 +121,65 @@ func pullCode() {
 		}
 	}
 }
-func buildImage() {
+func buildImage() int {
 	// put dir to #{CodeURL}.tar
 	tarFilePath := filepath.Join(WorkPath, "aim.tar")
 	createTar(WorkPath)
 	defer deleteFile(tarFilePath)
-	// send build req to Docker Daemon
+
+	dockerBuildContext, err := os.Open(tarFilePath)
+	defer dockerBuildContext.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	// set DOCKER_API_VERSION
+	os.Setenv("DOCKER_API_VERSION", strconv.FormatFloat(DockerAPIVersion, 'f', 2, 64))
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	options := types.ImageBuildOptions{
+		Tags:           []string{CodeURL},
+		NoCache:        false,
+		SuppressOutput: false,
+		Remove:         true,
+		ForceRemove:    true,
+		PullParent:     true,
+	}
+
 	// ctx, _ := context.WithCancel(context.Background())
 	ctx := context.Background()
-	if dockerBuildContext, err := os.Open(tarFilePath); err == nil {
-		defer dockerBuildContext.Close()
-		cli, err := client.NewEnvClient()
-		if err != nil {
-			fmt.Printf("%s", err.Error())
-		}
-		options := types.ImageBuildOptions{
-			Tags:           []string{CodeURL},
-			NoCache:        false,
-			SuppressOutput: false,
-			Remove:         true,
-			ForceRemove:    true,
-			PullParent:     true,
-		}
-		buildResponse, err := cli.ImageBuild(ctx, dockerBuildContext, options)
-		if err != nil {
-			fmt.Printf("%s", err.Error())
+	// send build req to Docker Daemon
+	buildResponse, err := cli.ImageBuild(ctx, dockerBuildContext, options)
+	if err != nil {
+		if res, _ := regexp.MatchString("too new", err.Error()); res == true {
+			return 1
 		} else {
-			// fmt.Printf("********* %s **********", buildResponse.OSType)
-			_, err := ioutil.ReadAll(buildResponse.Body)
-			if err != nil {
-				fmt.Printf("%s", err.Error())
-			} else {
-				// timeout := 5 * 60 * 1000
-				// timeout := 30 * 1000
-				// time.Sleep(time.Duration(timeout) * time.Millisecond)
-				// cancel()
-				_, err := ioutil.ReadAll(buildResponse.Body)
-				if err != nil {
-					fmt.Printf("%s", err.Error())
-				}
-				// fmt.Println(string(response))
-			}
+			panic(err)
+		}
+	} else {
+		// fmt.Printf("********* %s **********", buildResponse.OSType)
+		_, err := ioutil.ReadAll(buildResponse.Body)
+		if err != nil {
+			panic(err)
+		} else {
+			// timeout := 5 * 60 * 1000
+			// timeout := 30 * 1000
+			// time.Sleep(time.Duration(timeout) * time.Millisecond)
+			// cancel()
+			// response, err := ioutil.ReadAll(buildResponse.Body)
+			// if err != nil {
+			// fmt.Printf("%s", err.Error())
+			// } else {
+			// fmt.Println(string(response))
+			// }
+			// fmt.Println(string(response))
 		}
 	}
+	return 0
 }
+
 func checkImage() {
 
 }
--- test/stringFunction.go
+++ test/stringFunction.go
@@ -1,11 +1,19 @@
 package main
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 func convertURLToPath(url string) string {
 	// func Replace(s, old, new string, n int) string
 	return strings.Replace(url, "/", "_", -1)
 }
+func rege(s string) {
+	res, _ := regexp.MatchString("too new", s)
+	println(res)
+}
 func main() {
-	println(convertURLToPath("github.com/yxwzaxns/cider-ci-test"))
+	rege("Error response from daemon: client version 1.36 is too new. Maximum supported API version is 1.35")
+	// println(convertURLToPath("github.com/yxwzaxns/cider-ci-test"))
 }
